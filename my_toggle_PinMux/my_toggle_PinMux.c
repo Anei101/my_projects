c2fb0691ac04fca2fec79e6fc587ada32f711430
unknown <nachtwalpurgis@yahoo.com>
2020-03-07 16:02:24 -0800

    Final

--- my_toggle_PinMux/my_toggle_PinMux.c
+++ my_toggle_PinMux/my_toggle_PinMux.c
@@ -45,6 +45,14 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/gpio.h"
 
+#define 	GREEN_MASK 		0x08
+
+//*****************************************************************************
+//
+//!
+//! A very simple example that toggles the on-board green  LED using PinMux for 
+//! port initialization and functions in Peripheral Driver Library for port access
+//
 //*****************************************************************************
 void
 PortFunctionInit(void)
@@ -55,7 +63,41 @@ PortFunctionInit(void)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
     //
-    // Enable pin PF1 for GPIOOutput
+    // Enable pin PF3 for GPIOOutput
     //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
 }
+
+int main(void)
+{
+    uint8_t LED_data;
+	
+		//initialize the GPIO ports	
+		PortFunctionInit();
+	
+    // Turn on the LED.
+    LED_data= 0x08;
+		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, LED_data);
+    
+    //
+    // Loop forever.
+    //
+    while(1)
+    {
+        // Delay for a bit.
+				SysCtlDelay(2000000);	
+        // Delay for a bit.
+				SysCtlDelay(2000000);	
+        // Delay for a bit.
+				SysCtlDelay(2000000);	
+        // Delay for a bit.
+				SysCtlDelay(2000000);	
+        // Delay for a bit.
+				SysCtlDelay(2000000);	
+
+        // Toggle the LED.
+        LED_data^=GREEN_MASK;	//toggle red LED (PF1)
+				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, LED_data);
+    }
+}
+
